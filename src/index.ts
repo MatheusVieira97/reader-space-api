8ac7afb00379fed5a0ed7b9275f345ea1133af51
Matheus Vieira <matheus.msv1997@gmail.com>
2025-06-19 17:30:33 -0300

    chore: update Prettier and ESLint configurations

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
 
-import { middleware } from "./middlewares/middlewares.js";
+import { middleware } from './middlewares/middlewares.js';
 
 const app: Application = express();
-const port: string = process.env.PORT ?? "9001";
+const port: string = process.env.PORT ?? '9001';
 
-app.get("/", middleware);
+app.get('/', middleware);
 
 app.listen(port, () => {
   console.log(`API is listening on port ${port}`);
--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -1,10 +1,10 @@
 // middlewares.ts
-import { RequestHandler } from "express";
+import { RequestHandler } from 'express';
 
 export const middleware: RequestHandler = (req, res, next) => {
   try {
-    res.send("Hello World!");
-    console.log("Response sent");
+    res.send('Hello World!');
+    console.log('Response sent');
   } catch (error) {
     next(error as Error);
   }
